548cb0fde82e72bede2ef73f7a3bc51cb32e846d
Ribsa Salehudin <ribsasalehudin1704@gmail.com>
2023-02-22 15:26:52 +0700

    integrate email paid from admin

--- app/Http/Controllers/Admin/CheckoutController.php
+++ app/Http/Controllers/Admin/CheckoutController.php
@@ -5,7 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Checkout;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\Checkout\Paid;
 
 class CheckoutController extends Controller
 {
@@ -13,6 +14,11 @@ class CheckoutController extends Controller
     {
         $checkout->is_paid = true;
         $checkout->save();
+
+
+        // send email to user
+        Mail::to($checkout->User->email)->send(new Paid($checkout));
+
         $request->session()->flash('success', "Checkout with user {$checkout->User->name} has been updated!");
         return redirect(route('admin.dashboard'));
     }
---
+++ resources/views/emails/checkout/paid.blade.php
@@ -0,0 +1,15 @@
+@component('mail::message')
+# Your Transaction Has Been Confirmed
+
+Hi {{$checkout->user->name}}
+<br>
+Your transaction has been confirmed, now you can enjoy the benefits of <b> {{$checkout->Camps->title}}</b> camp.
+
+
+@component('mail::button', ['url' => route('user.dashboard')])
+My Dashboard
+@endcomponent
+
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
